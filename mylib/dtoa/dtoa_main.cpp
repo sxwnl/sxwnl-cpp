945a82e052640584df7d484fbd356767bb7e9c8a
qaiu <736226400@qq.com>
2023-04-06 15:21:34 +0800

    update fixed mylib

--- mylib/dtoa/dtoa_main.cpp
+++ mylib/dtoa/dtoa_main.cpp
@@ -1,9 +1,14 @@
 #include "./dtoa_milo.h"
 
+#include <stdio.h>
 
 int main() {
-    char ss[128]={};
-    milo::dtoa(65448566564654, ss);
+    char ss[32]={};
+    milo::dtoa(1.2345, ss, 2);
+    printf("-->%s\n", ss);s
+    milo::dtoa(23.1233, ss, 21);
+    printf("-->%s\n", ss);
+    milo::dtoa(23.44, ss, 3);
     printf("-->%s\n", ss);
     return 0;
 }
